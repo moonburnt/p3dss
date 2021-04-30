ded5e72676a94d21ffbc4f0c10c0eca389de640f
moonburnt <moonburnt@disroot.org>
2021-04-30 15:20:20 +0300

    style: setup.py improvements

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
-from os.path import join
-from setuptools import setup
+from setuptools import find_packages, setup
 
-VERSION = 0.1
+VERSION = "0.1.1"
 
-WORKDIR = "."
-README = join(WORKDIR, "README.md")
-DESCRIPTION_TYPE = "text/markdown"
-
-# Iirc python's default encoding is already utf-8, but just in case
-with open(README, encoding = 'utf-8') as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
-    name = "p3dss",
-    version = VERSION,
-    description = "p3dss - the spritesheet handling library for Panda3D",
-    long_description = long_description,
-    long_description_content_type = DESCRIPTION_TYPE,
-    url = "https://github.com/moonburnt/p3dss",
-    author = "moonburnt",
-    license = "MIT",
-    classifiers = [ "Programming Language :: Python :: 3" ],
-    packages = [ 'p3dss' ],
-    install_requires=[ "panda3d>=1.10" ]
+    name="p3dss",
+    version=VERSION,
+    description="p3dss - the spritesheet handling library for Panda3D",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moonburnt/p3dss",
+    author="moonburnt <moonburnt@disroot.org",
+    license="MIT",
+    classifiers=["Programming Language :: Python :: 3"],
+    packages=find_packages(),
+    install_requires=["panda3d>=1.10"],
     )
